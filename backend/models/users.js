1b982025e84fda9213f54596334e6026e379559b
Sam Verdejo <sjverdejo@gmail.com>
2023-08-15 09:49:07 -0400

    added mongoose unique validator and updating model accordingly for users

--- backend/models/users.js
+++ backend/models/users.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   passwordHash: {
     type: String,
