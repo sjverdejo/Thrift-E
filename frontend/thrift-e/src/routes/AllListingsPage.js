3ace9cee849e078e4105f23a03467a521029fd5e
Sam Verdejo <sjverdejo@gmail.com>
2023-08-25 11:28:05 -0400

    Removed state from component

--- frontend/thrift-e/src/routes/AllListingsPage.js
+++ frontend/thrift-e/src/routes/AllListingsPage.js
@@ -1,15 +1,13 @@
 import { useOutletContext } from "react-router-dom"
-import { useState } from 'react'
 import Items from '../components/items/Items'
 
 const AllListingsPage = () => {
   const { user, allItems } = useOutletContext()
-  const [itemList, setItemList] = useState(allItems.filter((i) => (i.seller.toString() !== user.user._id)))
   
   return (
     <div>
-      <h1>Items</h1>
-        <Items items={itemList} />
+      <h1>All Items</h1>
+        <Items items={allItems.filter((i) => (i.seller.toString() !== user.user._id))} />
     </div>
   )
 }
