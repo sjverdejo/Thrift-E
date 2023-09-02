8756f6ce27baa7bca44d6485e4ec6d86a6a74ac3
Sam Verdejo <sjverdejo@gmail.com>
2023-09-02 14:44:34 -0400

    Added delete functionality for items

--- frontend/thrift-e/src/components/items/UpdateListingForm.js
+++ frontend/thrift-e/src/components/items/UpdateListingForm.js
@@ -7,6 +7,8 @@ const UpdateListingForm = ({item, setAlertMessage}) => {
   const [name, setName] = useState(item.name)
   const [price, setPrice] = useState(item.price)
   const [clothingType, setClothingType] = useState(item.clothingType)
+  const [promptDelete, setPromptDelete] = useState(false)
+  const [confirmDelete, setConfirmDelete] = useState('')
 
   const updateListing = (e) => {
     e.preventDefault()
@@ -37,27 +39,54 @@ const UpdateListingForm = ({item, setAlertMessage}) => {
     }
   }
 
+  const deleteItem = (e) => {
+    e.preventDefault()
+    
+    if (confirmDelete === item.name) {
+      ItemsAPI.deleteItem(item._id)
+        .then(res => {
+          setAlertMessage('Deleted item successfully.')
+          navigate(`/user/${item.seller}`)
+        })
+        .catch(err => {
+          setAlertMessage('Something went wrong!')
+        })
+    } else {
+      setAlertMessage('Input was incorrect.')
+    }
+  }
+
   return (
-    <form onSubmit={updateListing}>
-      Listing Name:<input value={name} onChange={({target}) => setName(target.value)} required/>
-      Price: <input value={price} onChange={({target}) => setPrice(target.value)} required/>
-      Clothing Type: 
-      <select 
-        value={clothingType}
-        onChange={({target}) => setClothingType(target.value)}
-        required>
-        <option value='Tanktop'>Tanktop</option>
-        <option value='Shirt'>Shirt</option>
-        <option value='Sweater'>Sweater</option>
-        <option value='Dress'>Dress</option>
-        <option value='Pants'>Pants</option>
-        <option value='Socks'>Socks</option>
-        <option value='Hat'>Hat</option>
-        <option value='Shoes'>Shoes</option>
-        <option value='Jacket'>Jacket</option>
-      </select>
-      <input type='submit' value='Post Item Listing'/>
-    </form>
+    <div>
+      <form onSubmit={updateListing}>
+        Listing Name:<input value={name} onChange={({target}) => setName(target.value)} required/>
+        Price: <input value={price} onChange={({target}) => setPrice(target.value)} required/>
+        Clothing Type: 
+        <select 
+          value={clothingType}
+          onChange={({target}) => setClothingType(target.value)}
+          required>
+          <option value='Tanktop'>Tanktop</option>
+          <option value='Shirt'>Shirt</option>
+          <option value='Sweater'>Sweater</option>
+          <option value='Dress'>Dress</option>
+          <option value='Pants'>Pants</option>
+          <option value='Socks'>Socks</option>
+          <option value='Hat'>Hat</option>
+          <option value='Shoes'>Shoes</option>
+          <option value='Jacket'>Jacket</option>
+        </select>
+        <input type='submit' value='Update Item Listing'/>
+      </form>
+      <button onClick={()=>setPromptDelete(!promptDelete)}>Delete Listing?</button>
+      { promptDelete &&
+        <form onSubmit={deleteItem}>
+          <h3>Type item name to delete permanently</h3>
+          <input value={confirmDelete} onChange={({target}) => setConfirmDelete(target.value)} />
+          <input type='submit' value='Delete Item Permanently' />
+        </form>
+      }
+    </div>
   )
 
 }
