31af9eb0319917c6ebd6aca606256bed8155c08f
Sam Verdejo <sjverdejo@gmail.com>
2023-09-04 14:56:23 -0400

    Completed transactions css

--- frontend/thrift-e/src/components/checkout/Transaction.js
+++ frontend/thrift-e/src/components/checkout/Transaction.js
@@ -4,26 +4,28 @@ import helper from '../../helper'
 const Transaction = ({transaction, isSeller}) => {
   const seller = () => {
     return (
-      <div>
-        <h4>Date Sold: {helper.convertDate(transaction.datePurchased)}</h4>
-        <Link to={`/user/${transaction.buyer}`}><button>View Buyer Profile</button></Link>
+      <div class='flex justify-between'>
+        <h4>Sold: {helper.convertDate(transaction.datePurchased)}</h4>
+        <Link to={`/user/${transaction.buyer}`}><button>Buyer Profile</button></Link>
       </div>
     )
   }
 
   const buyer = () => {
     return (
-      <div>
-        <h4>Date Purchased: {helper.convertDate(transaction.datePurchased)}</h4>
-        <Link to={`/user/${transaction.item.seller}`}><button>View Seller Profile</button></Link>
+      <div class='flex justify-between'>
+        <h4>Bought: {helper.convertDate(transaction.datePurchased)}</h4>
+        <Link to={`/user/${transaction.item.seller}`}><button>Seller Profile</button></Link>
       </div>
     )
   }
 
   return (
-    <div>
-      <h1>{transaction.item.name}</h1>
-      <h3>{transaction.item.price}</h3>
+    <div class='border bg-slate-600 w-96 p-2 rounded-lg shadow-lg'>
+      <div class='flex justify-between'>
+        <h1 class='text-2xl'>{transaction.item.name}</h1>
+        <h1 class='text-2xl'>${transaction.item.price}</h1>
+      </div>
       {
         isSeller
           ? seller()
